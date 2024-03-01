4ae28a97795a0f41bf23f115d279c41a6bc085fc
hrsk <hrsk.yegor@gmail.com>
2024-03-01 16:18:43 +0300

    app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import React from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className={"app-wrapper"}>
+      <Header />
+      <Navbar />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export const Header = () => {
+  return (
+    <div className='header'>
+      HEADER
+    </div>
+  );
+}
+export const Navbar = () => {
+  return (
+    <div className='nav'>
+      NAVBAR
+    </div>
+  );
+}
+export const Content = () => {
+  return (
+    <div className='app-wrapper-content'>
+      CONTENT
+      <Profile />
+      <Dialogs />
+    </div>
+  );
+}
+export const Footer = () => {
+  return (
+    <div className='footer'>
+      FOOTER
+    </div>
+  );
+}
+export const Profile = () => {
+  return (
+    <div>
+      PROFILE
+    </div>
+  );
+}
+
+export const Dialogs = () => {
+  return (
+    <div>
+      DIALOGS
+    </div>
+  );
+}
