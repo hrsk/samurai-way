4b4e7f5049127025663d7f8c588aa89b44d4af86
hrsk <hrsk.yegor@gmail.com>
2024-04-04 22:09:55 +0300

    select user profile

--- src/components/profile/ProfileContainer.tsx
+++ src/components/profile/ProfileContainer.tsx
@@ -3,9 +3,23 @@ import { AppStateType } from "../../store/redux-store";
 import { ConnectedProps, connect } from "react-redux";
 import { Profile } from "./Profile";
 import { addPost, changePostText, getUserProfile } from "../reducers/profileReducer";
-import { PostType } from "../../types";
+import { PostType, UserProfileType } from "../../types";
+import axios from "axios";
+import { fetching } from "../reducers/appReducer";
+import { Preloader } from "../../features/Preloader";
 
 export class ProfileConnectedComponent extends React.Component<ConnectedPropsType, AppStateType> {
+
+    componentDidMount(): void {
+        const userId = 2
+
+        this.props.fetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+            .then((response) => {
+                this.props.getUserProfile(response.data)
+                this.props.fetching(false)
+            })
+    }
 
     changePostText = (value: string) => {
         this.props.changePostText(value)
@@ -16,20 +30,27 @@ export class ProfileConnectedComponent extends React.Component<ConnectedPropsTyp
     }
 
     render() {
-        return <Profile {...this.props} />
+        return (
+            this.props.isFetching
+                ? <Preloader />
+                : <Profile {...this.props} />
+        )
     }
 }
 
 type MapStateToPropsType = {
     posts: PostType[]
     postText: string
-
+    userProfile: UserProfileType
+    isFetching: boolean
 }
 
 const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
     return {
         posts: state.profilePage.posts,
         postText: state.profilePage.postText,
+        userProfile: state.profilePage.user,
+        isFetching: state.app.isFetching,
     }
 }
 
@@ -37,6 +58,6 @@ interface OwnProps extends ClassAttributes<ProfileConnectedComponent> { }
 type PropsFromRedux = ConnectedProps<typeof connector>
 export type ConnectedPropsType = MapStateToPropsType & PropsFromRedux & OwnProps
 
-const connector = connect(mapStateToProps, { getUserProfile, changePostText, addPost });
+const connector = connect(mapStateToProps, { getUserProfile, changePostText, addPost, fetching });
 
 export const ProfileContainer = connector(ProfileConnectedComponent)
