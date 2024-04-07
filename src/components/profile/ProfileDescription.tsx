131d2dbcd17d62cf4deda1c4601b24deb5639b39
hrsk <hrsk.yegor@gmail.com>
2024-04-07 14:34:55 +0300

    fix render profile description component

--- src/components/profile/ProfileDescription.tsx
+++ src/components/profile/ProfileDescription.tsx
@@ -10,9 +10,12 @@ export const ProfileDescription = (props: PropsType) => {
                 <p>{props.userProfile.userId}</p>
                 <span style={{ display: 'block' }}>{props.userProfile.aboutMe}</span>
                 <span style={{ display: 'block', fontWeight: 600 }}>{props.userProfile.fullName}</span>
-                <img style={{ width: '64px', height: '64px' }}
-                    src={props.userProfile.photos.small ? props.userProfile.photos.large : no_avatar}
-                    alt="" />
+                {props.userProfile.photos ?
+                    <img style={{ width: '64px', height: '64px' }}
+                        src={props.userProfile.photos.small && props.userProfile.photos.large}
+                        alt="" />
+                    : <img style={{ width: '64px', height: '64px' }} src={no_avatar} alt="" />
+                }
             </div>
             <div>
                 <span style={{ display: 'block' }}>
@@ -24,42 +27,46 @@ export const ProfileDescription = (props: PropsType) => {
                     <p style={{ display: 'inline-block' }}>{props.userProfile.lookingForAJobDescription}</p>
                 </span>
             </div>
-            <div>
-                <span style={{ display: 'block', fontSize: 24, fontWeight: 600 }}>Contacts:</span>
-                <span style={{ display: 'block' }}>
-                    facebook:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.facebook}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    website:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.website}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    vk:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.vk}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    twitter:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.twitter}</p>
-                </span>
+            {
+                props.userProfile.contacts ?
+                    <div>
+                        <span style={{ display: 'block', fontSize: 24, fontWeight: 600 }}>Contacts:</span>
+                        <span style={{ display: 'block' }}>
+                            facebook:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.facebook}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            website:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.website}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            vk:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.vk}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            twitter:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.twitter}</p>
+                        </span>
 
-                <span style={{ display: 'block' }}>
-                    instagram:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.instagram}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    youtube:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.youtube}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    github:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.github}</p>
-                </span>
-                <span style={{ display: 'block' }}>
-                    mainLink:
-                    <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.mainLink}</p>
-                </span>
-            </div>
+                        <span style={{ display: 'block' }}>
+                            instagram:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.instagram}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            youtube:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.youtube}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            github:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.github}</p>
+                        </span>
+                        <span style={{ display: 'block' }}>
+                            mainLink:
+                            <p style={{ display: 'inline-block' }}>{props.userProfile.contacts.mainLink}</p>
+                        </span>
+                    </div>
+                    : <div>is empty</div>
+            }
         </div>
     )
 }
