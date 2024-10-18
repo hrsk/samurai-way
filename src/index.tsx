c22b77d539510b8113ae0bf6707cf46e505ab193
hrsk <hrsk.yegor@gmail.com>
2024-10-18 14:53:33 +0300

    custom provider component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { FC } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import './index.css'
 import { store } from './store/redux-store'
 
-export const MyContext = React.createContext(store)
+const CustomProviderComponent: FC = ({ children }) => {
+	return <MyContext.Provider value={store}>{children}</MyContext.Provider>
+}
+export const MyContext = React.createContext<any>(store)
 
 let rerenderEntireThree = () => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<MyContext.Provider value={store}>
+			<CustomProviderComponent>
 				<App />
-				{/* <App state={store.getState()} dispatch={store.dispatch.bind(store)} /> */}
-			</MyContext.Provider>
+			</CustomProviderComponent>
+
+			{/* <MyContext.Provider value={store}> */}
+
+			{/* <App state={store.getState()} dispatch={store.dispatch.bind(store)} /> */}
+			{/* </MyContext.Provider> */}
 		</BrowserRouter>,
 		document.getElementById('root')
 	)
