3b45bee05c75f387b7b79457a297eeb9085598b4
hrsk <hrsk.yegor@gmail.com>
2024-11-01 19:20:48 +0300

    next & prev page and pages portion handlers

--- src/pagination/Pagination.tsx
+++ src/pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type PropsType = {
     usersCount: number
@@ -21,17 +21,46 @@ export const Pagination = (props: PropsType) => {
         props.changePage(pN)
     }
 
+    let [pagesOutput, setPagesOutput] = useState<number>(1);
+
+    let pagesInLine = 7;
+
+    const prevCountPageOutput = (pagesOutput - 1) * pagesInLine + 1;
+    const nextCountPageOutput = pagesOutput * pagesInLine;
+
+    const prevPortionPages = () => {
+        setPagesOutput(pagesOutput - 1)
+    }
+
+    const nextPortionPages = () => {
+        setPagesOutput(pagesOutput + 1)
+    }
+
+    const nextPageHandler = () => {
+        selectPage(props.currentPage + 1)
+        if (props.currentPage % pagesInLine === 0) {
+            nextPortionPages()
+        }
+    }
+
+    const prevPageHandler = () => {
+        selectPage(props.currentPage - 1)
+        if ((props.currentPage - 1) % pagesInLine === 0) {
+            prevPortionPages()
+        }
+    }
+
     return (
         <div>
-
-            <button>{'<<'}</button>
-            <button>prevPage</button>
+            <button onClick={prevPortionPages}>{'<<'}</button>
+            <button onClick={prevPageHandler}>prevPage</button>
             {
-                pages.map((page: number) => <button style={props.currentPage === page ? {color: 'red'} : {}}
-                                                    onClick={() => selectPage(page)}>{page}</button>)
+                pages.filter(page => page >= prevCountPageOutput && page <= nextCountPageOutput)
+                    .map((page: number) => <button style={props.currentPage === page ? {color: 'red'} : {}}
+                                                   onClick={() => selectPage(page)}>{page}</button>)
             }
-            <button>nextPage</button>
-            <button>{'>>'}</button>
+            <button onClick={nextPageHandler}>nextPage</button>
+            <button onClick={nextPortionPages}>{'>>'}</button>
         </div>
     );
 };
